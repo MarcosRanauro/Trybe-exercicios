a11f11993910ddb60b4cb422d9e1c2fec5b87bfd
MarcosRanauro <marcosranauro@hotmail.com>
2022-09-21 20:32:20 -0300

    Segundo exercicio

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio2.js
@@ -0,0 +1,11 @@
+const num1 = 4;
+const num2 = 5;
+const num3 = 6;
+
+if(num1 > num2 && num1 > num3){
+    console.log("Num1 venceu");
+}else if(num2 > num1 && num2 > num3){
+    console.log("Num2 venceu");
+} else {
+    console.log("Num3 venceu");
+}
