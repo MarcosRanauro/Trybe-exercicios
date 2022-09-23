4756a90c1249c136239f6cf460b803a6d20629d9
MarcosRanauro <marcosranauro@hotmail.com>
2022-09-23 01:50:01 -0300

    Fiz todos os exercicios do dia 4.2

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercicio9.js
@@ -0,0 +1,5 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1){
+    console.log("Numero " + numbers[index] + " dividido por 2 = " + numbers[index] / 2);
+}
