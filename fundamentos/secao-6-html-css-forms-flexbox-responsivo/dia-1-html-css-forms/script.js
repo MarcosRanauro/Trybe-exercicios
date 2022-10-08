500a3cbfc127981428fcb8ec21450d1fc322288f
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-08 18:09:35 -0300

    Atualização 8.10

--- fundamentos/secao-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/secao-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/script.js
@@ -1,19 +1,19 @@
 const getSendButton = document.getElementById("sendButton");
 
-getSendButton.addEventListener('click', function (event){
-    event.preventDefault();    
+getSendButton.addEventListener('click', function (event) {
+    event.preventDefault();
     const getName = document.getElementById("full-name");
     const getEmail = document.getElementById("email");
     const getTextArea = document.getElementById("textArea");
 
     if (getName.value.length < 10 || getEmail.value.length < 10 || getTextArea.value.length < 10) {
-        alert ("Dados invalidos");
+        alert("Dados invalidos");
     } else {
-        alert ('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip!')
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip!')
     }
 })
 
-function enableSubmit () {
+function enableSubmit() {
     const getSubmit = document.getElementById("sendButton");
     const getCheckbox = document.getElementById("verificationIMG");
     getSubmit.disabled = !getCheckbox.checked;
@@ -24,5 +24,3 @@ window.onload = function () {
     const agreement = document.querySelector('#verificationIMG');
     agreement.addEventListener('change', enableSubmit);
 }
-
-    
