ccca0b51ba76f8ad9056abef4236cf9f5a8b8bd3
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-24 19:09:44 -0300

    Exercicio 1 corrigido

--- fundamentos/secao-8-higher-order-functions-do-javaScript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/Exercicio 1/script.js
+++ fundamentos/secao-8-higher-order-functions-do-javaScript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/Exercicio 1/script.js
@@ -3,11 +3,11 @@ const employeeGenerator = (fullName) => {
     return { fullName, email: `${email}@trybe.com` };
   };
 
-const newEmployees = (callback) => {
+const newEmployees = (employeeGenerator) => {
     const employees = {
-      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback('Luiza Drummond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: employeeGenerator('Luiza Drummond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
