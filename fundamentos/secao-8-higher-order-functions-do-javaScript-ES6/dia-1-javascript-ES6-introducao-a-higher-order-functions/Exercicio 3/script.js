20bdee01ffdbe9eac4556dd9ea092bdacfc459d1
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-24 19:19:51 -0300

    Finalizei o exercicio 3

---
+++ fundamentos/secao-8-higher-order-functions-do-javaScript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/Exercicio 3/script.js
@@ -0,0 +1,19 @@
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let counter = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    counter += actionReturn;
+  }
+  return `Resultado final: ${counter} pontos`;
+};
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
