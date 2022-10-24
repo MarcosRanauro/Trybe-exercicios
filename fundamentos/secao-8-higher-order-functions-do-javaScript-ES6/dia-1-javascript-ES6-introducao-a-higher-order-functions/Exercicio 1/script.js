3f6a8b547ee11582f73eb7d46dacda9eca13f640
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-24 18:55:30 -0300

    Finalizei o exercicio 1]

---
+++ fundamentos/secao-8-higher-order-functions-do-javaScript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/Exercicio 1/script.js
@@ -0,0 +1,15 @@
+const employeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${email}@trybe.com` };
+  };
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drummond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+  console.log(newEmployees(employeeGenerator));
